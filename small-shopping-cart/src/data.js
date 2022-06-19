18dbc7344bf32ba3acf3a0a77ef025117dd69141
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-06-20 00:09:05 +0200

    Added small shopping cart

---
+++ small-shopping-cart/src/data.js
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'MacBook',
+      price: 14000,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8m63P2FMDwPvk4m1JJAvQWyHmf-HLpqizCg&usqp=CAU',
+    },
+    {
+      id: '2',
+      name: 'Old Car',
+      price: 100000,
+      image: 'https://cdn.thecoolist.com/wp-content/uploads/2017/02/Chevy-Camaro-1967-69-vintage-car.jpg',
+    },
+    {
+      id: '3',
+      name: 'Womens Shoes',
+      price: 1200,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzstyvPiTk3UHRqEgyH4poqGiA570Vi_aI_g&usqp=CAU',
+    },
+
+    {
+        id: '4',
+        name: 'Wedding Gown',
+        price: 30000,
+        image: 'https://static01.nyt.com/images/2020/10/07/fashion/07BLACKBRIDALDESIGNERS-COMBO/07BLACKBRIDALDESIGNERS-COMBO-jumbo.jpg?quality=75&auto=webp',
+      },
+  ],
+};
+export default data;
