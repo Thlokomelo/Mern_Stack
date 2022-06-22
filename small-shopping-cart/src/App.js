a5695c118fa5d0f1fae79685c110c8d3ec4b491d
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-06-22 22:53:38 +0200

    Added Proshop project

--- small-shopping-cart/src/App.js
+++ small-shopping-cart/src/App.js
@@ -3,26 +3,27 @@ import Main from './components/Main';
 import Basket from './components/Basket';
 import data from './data';
 import { useState } from 'react';
+
 function App() {
   const { products } = data;
   const [cartItems, setCartItems] = useState([]);
   const onAdd = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
-    if (exist) {
+    if (exist) {  // if already in basket then increase the qty 
       setCartItems(
         cartItems.map((x) =>
           x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
         )
       );
-    } else {
-      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    } else { //else if not in basket put in basket qty 1
+      setCartItems([...cartItems, { ...product, qty: 1 }]); //updates the basket
     }
   };
-  const onRemove = (product) => {
+  const onRemove = (product) => { 
     const exist = cartItems.find((x) => x.id === product.id);
-    if (exist.qty === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== product.id));
-    } else {
+    if (exist.qty === 1) { //else if in basket with a qty of 1 then remove from basket
+      setCartItems(cartItems.filter((x) => x.id !== product.id)); //updates the basket
+    } else { // it is in the basket with a qty > 1, then reduce the qty by 1
       setCartItems(
         cartItems.map((x) =>
           x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
